93ecbbb9e7fddf4eac8c8150bdffe67badfeee94
mesterhammerfic <mesterhammerfic@gmail.com>
2020-05-27 12:02:01 -0500

    Added 2018 data import scripts and notebook

---
+++ src/data/data_collection_2018.py
@@ -0,0 +1,90 @@
+from io import BytesIO, TextIOWrapper, StringIO
+from zipfile import ZipFile
+from gzip import GzipFile
+from csv import QUOTE_ALL
+
+import pandas as pd
+import requests
+
+from src.data import sql_utils_2018
+
+def download_data_and_load_into_sql():
+    """
+    This function dispatches everything.  It creates a PostgreSQL database with
+    the appropriate name, sets up the table schema, downloads all of the files
+    containing the data, and loads the data into the database
+    """
+    sql_utils_2018.create_tables()
+    data_files_dict = collect_all_data_files()
+    load_into_sql(data_files_dict)
+
+
+def collect_all_data_files():
+    """
+    Create a dictionary with the in-memory file objects associated with all
+    database tables
+    """
+    data_files_dict = {
+        "pums_2018": collect_pums_2018_data()
+    }
+    return data_files_dict
+
+
+def load_into_sql(data_files_dict):
+    """
+    Given a dictionary of in-memory file objects, use sql_utils to copy them
+    into the database.  Then close all of them.
+
+    Each dictionary value is a tuple containing a CSV file object, then either
+    None or some other file to be closed, e.g. a zip file
+    """
+    sql_utils_2018.copy_csv_files(data_files_dict)
+
+    for csv_file, other_file in data_files_dict.values():
+        csv_file.close()
+        if other_file:
+            other_file.close()
+
+
+def collect_pums_2018_data():
+    """
+    Download the 2018 5-year ACS PUMS person-level records for the state of WA
+    """
+    PUMS_2018_URL = "https://www2.census.gov/programs-surveys/acs/data/pums/2018/5-Year/csv_pwa.zip"
+    PUMS_2018_CSV_NAME = "psam_p53.csv"
+    return collect_zipfile_data(PUMS_2018_URL, PUMS_2018_CSV_NAME)
+
+
+def collect_zipfile_data(URL, csv_name):
+    """
+    Helper function used to collect CSV files contained in .zip archives
+    """
+    zip_file = download_zipfile(URL)
+    csv_file = open_csv_from_zip(zip_file, csv_name)
+    # return both so we can safely close them at the end
+    return csv_file, zip_file
+
+
+def download_zipfile(URL):
+    """
+    Given a URL for a .zip, download and unzip the .zip file
+    """
+    response = requests.get(URL)
+    print(f"""Successfully downloaded ZIP file
+    {URL}
+    """)
+
+    content_as_file = BytesIO(response.content)
+    zip_file = ZipFile(content_as_file)
+    return zip_file
+
+
+def open_csv_from_zip(zip_file, csv_name):
+    """
+    Given an unzipped .zip file and the name of a CSV inside of it, 
+    extract the CSV and return the relevant file
+    """
+    csv_file_bytes = zip_file.open(csv_name)
+    # it seems we have to open the .zip as bytes, but CSV reader requires text
+    csv_file_text = TextIOWrapper(csv_file_bytes, encoding="ISO-8859-1")
+    return csv_file_text
---
+++ src/data/sql_utils_2018.py
@@ -0,0 +1,100 @@
+import psycopg2
+import pandas as pd
+import os
+
+DBNAME = "opportunity_youth"
+
+#Retrieving postgres info for MEF
+# from src.data import local
+# PORT = local.port
+# USER = local.user
+# HOST = local.host
+# PASSWORD = local.password
+
+
+def create_tables():
+    """
+    Composite function that creates all relevant tables in the database
+    This creates empty tables with the appropriate schema, then the data
+    transfer is performed in the `copy_csv_files` function
+    """
+    # Depending on your local settings, you may need to specify a user and password, e.g.
+    # conn = psycopg2.connect(dbname=DBNAME, user="postgres", password="password")
+    # conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, port=PORT, host=HOST)
+    conn = psycopg2.connect(dbname=DBNAME)
+
+    create_pums_2018_table(conn)
+    conn.close()
+
+
+def create_pums_2018_table(conn):
+    """
+    Create a table for the 2018 5-year persons PUMS data
+    """
+    execute_sql_script(conn, "create_pums_2018_table.sql")
+
+def copy_csv_files(data_files_dict):
+    """
+    Composite function that copies all CSV files into the database
+    """
+    # Depending on your local settings, you may need to specify a user and password, e.g.
+    # conn = psycopg2.connect(dbname=DBNAME, user="postgres", password="password")
+    # conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, port=PORT, host=HOST)
+    conn = psycopg2.connect(dbname=DBNAME)
+
+    for name, files in data_files_dict.items():
+        csv_file = files[0]
+        # skip the header; this info is already in the table schema
+        next(csv_file)
+        if name == "pums_2018":
+            copy_csv_to_pums_2018_table(conn, csv_file)
+
+        print(f"""Successfully loaded CSV file into `{name}` table
+        """)
+
+    conn.close()
+
+
+def copy_csv_to_pums_2018_table(conn, csv_file):
+    """
+    Copy the CSV contents of the 2018 5-year persons data into the table
+    """
+    COPY_PUMS_2018 = "copy_pums_2018_to_table.psql"
+    copy_expert_psql_script(conn, COPY_PUMS_2018, csv_file)
+    
+
+def execute_sql_script(conn, script_filename):
+    """
+    Given a DB connection and a file path to a SQL script, open up the SQL
+    script and execute it
+    """
+    file_contents = open_sql_script(script_filename)
+    cursor = conn.cursor()
+    cursor.execute(file_contents)
+    conn.commit()
+
+
+def open_sql_script(script_filename):
+    """
+    Given a file path, open the file and return its contents
+    We assume that the file path is always inside the sql directory
+    """
+    dir = os.path.dirname(__file__)
+    relative_filename = os.path.join(dir, 'sql', script_filename)
+
+    file_obj = open(relative_filename, 'r')
+    file_contents = file_obj.read()
+    file_obj.close()
+
+    return file_contents
+
+
+def copy_expert_psql_script(conn, script_filename, csv_file):
+    """
+    Given a DB connection and a file path to a PSQL script, open up the PSQL
+    script and use it to run copy_expert
+    """
+    file_contents = open_sql_script(script_filename)
+    cursor = conn.cursor()
+    cursor.copy_expert(file_contents, csv_file)
+    conn.commit()
